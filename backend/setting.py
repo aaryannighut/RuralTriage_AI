a6df672ebb883e74555c69180b5649104ccfbaad
Aaryan Nighut <aaryannighut07@gmail.com>
2026-04-16 19:26:06 +0530

    fix: set default values for LOG_LEVEL and DEBUG in Settings class

--- backend/setting.py
+++ backend/setting.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    LOG_LEVEL: str
-    DEBUG: bool
+    LOG_LEVEL: str = "info"
+    DEBUG: bool = True
     DATABASE_URL: str
     GROQ_API_KEY: str = ""
 
